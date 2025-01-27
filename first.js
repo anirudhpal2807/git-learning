fdcc194a895610e2093c00cec58d395ea9608bfd
Anirudh pal <palanirudh82992@gmail.com>
2025-01-27 12:45:54 +0530

    latest update

--- first.js
+++ first.js
@@ -4,5 +4,7 @@ const login="login page added";
 console.log(login);
 const payment="payment are intigrated";
 console.log(payment);
-const footer="footer added to our website";
+const footer="footer added to our git website";
 console.log(footer);
+console.log("latestupdate");
+
