6ef699c44d03654c6ffb6ceb73e7005ceccc04a9
Anirudh pal <palanirudh82992@gmail.com>
2025-01-27 10:22:24 +0530

    add

--- first.js
+++ first.js
@@ -2,3 +2,7 @@ const button="added a button";
 console.log(button);
 const login="login page added";
 console.log(login);
+const payment="payment are intigrated";
+console.log(payment);
+const footer="footer added to our website";
+console.log(footer);
